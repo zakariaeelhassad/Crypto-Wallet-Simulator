8c2e80fc96dd04c318564f234337423d2ba2b4f1
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-09-28 18:17:08 +0100

    Ajouter ValidationAddress pour vérifier le format des adresses BTC et ETH

--- src/utili/ValidationAddress.java
+++ src/utili/ValidationAddress.java
@@ -1,6 +1,34 @@
     package utili;
 
-    public class ValidationAddress {
+    import models.enums.CryptoType;
 
+    public final class ValidationAddress {
 
+        private ValidationAddress() {}
+
+        public static boolean isBtcAddress(String address){
+            if(address==null){
+                return false;
+            }else{
+                return address.startsWith("0x");
+            }
+        }
+
+        public static boolean isEthAddress(String address){
+            if(address==null){
+                return false;
+            }else{
+                return address.startsWith("bc1");
+            }
+        }
+
+        public static CryptoType getCryptoType(String address){
+            if (isBtcAddress(address)) {
+                return CryptoType.BITCOIN;
+            }else if (isEthAddress(address)) {
+                return CryptoType.ETHEREUM;
+            }else {
+                return null ;
+            }
+        }
     }
